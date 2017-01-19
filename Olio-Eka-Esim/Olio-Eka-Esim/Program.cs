684ab01c6179bf5e8f56bf05ba142d4c345186d2
h8324 <h8324@OHJE-330-WS-29>
2017-01-19 10:30:42 +0200

    Initial commit.

---
+++ Olio-Eka-Esim/Olio-Eka-Esim/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Eka_Esim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // määritellään muuttuja 
+            string nimi = "Pepe";
+            // tulostetaan muuttujan arvo konsolille
+            Console.WriteLine("C'est ne pas un " + nimi);
+            // pidetään sovelluksen ajoikkuna näytöllä. Sulkeutuu, kun käyttäjä painaa Enter-painiketta
+            Console.ReadLine();
+        }
+    }
+}
